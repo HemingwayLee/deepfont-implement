7998406815620be0d9b35b1cd642ab37a21463b3
LeeHemingway <coherence0815@gmail.com>
2022-11-28 22:41:27 +0900

    refactoring

--- eval.py
+++ eval.py
@@ -1,24 +1,11 @@
 # from matplotlib.pyplot import imshow
 # import matplotlib.cm as cm
 # import matplotlib.pylab as plt
-import os
-import random
 import PIL
-import cv2
 import argparse
-import itertools
 import numpy as np
-from imutils import paths
 from tensorflow.keras.utils import img_to_array
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
-from keras import callbacks, optimizers
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import BatchNormalization
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D , UpSampling2D ,Conv2DTranspose
-from keras import backend as K
 
 def rev_conv_label(label):
     if label == 0 :
@@ -44,11 +31,9 @@ if __name__ == '__main__':
     model = load_model('top_model.h5')
     # y = model.predict_classes(data)
     predict_y=model.predict(data) 
-    print("1")
     print(predict_y)
 
     classes_y=np.argmax(predict_y, axis=1)
-    print("2")
     print(classes_y)
 
     label = rev_conv_label(int(classes_y))
